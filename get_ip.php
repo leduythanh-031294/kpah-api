f06fe96f0b0136e52f7e0bd8787e9f9f9a7f6907
leduythanh-031294 <leduythanh.donga@gmail.com>
2025-11-14 22:01:57 +0700

    Update get_ip.php

--- get_ip.php
+++ get_ip.php
@@ -1,27 +1,21 @@
 <?php
+// get_ip.php
 error_reporting(0);
-header("Content-Type: text/plain; charset=UTF-8");
+header('Content-Type: text/plain; charset=UTF-8');
 
-function get_ip() {
+function client_ip() {
     $keys = [
-        "HTTP_CF_CONNECTING_IP",
-        "HTTP_X_FORWARDED_FOR",
-        "HTTP_X_REAL_IP",
-        "HTTP_CLIENT_IP",
-        "REMOTE_ADDR"
+        'HTTP_CF_CONNECTING_IP', 'HTTP_TRUE_CLIENT_IP', 'HTTP_X_FORWARDED_FOR',
+        'HTTP_X_REAL_IP', 'HTTP_CLIENT_IP', 'REMOTE_ADDR'
     ];
-
     foreach ($keys as $k) {
         if (!empty($_SERVER[$k])) {
             $ip = $_SERVER[$k];
-            if (strpos($ip, ",") !== false)
-                $ip = trim(explode(",", $ip)[0]);
-
-            if (filter_var($ip, FILTER_VALIDATE_IP))
-                return $ip;
+            if (strpos($ip, ',') !== false) $ip = trim(explode(',', $ip)[0]);
+            if (filter_var($ip, FILTER_VALIDATE_IP)) return $ip;
         }
     }
-    return "0.0.0.0";
+    return '0.0.0.0';
 }
 
-echo get_ip();
+echo client_ip();
